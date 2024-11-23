7e43d08fe48ee1ea30a42bae003048723415f6e9
amit013021 <amit013021@gmail.com>
2024-11-24 01:04:18 +0530

    add react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
  import "./index.css"
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RegisterPage from './pages/register.tsx'
+import LoginPage from './pages/Login.tsx'
+import Homepage from './pages/Home.tsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage/>,
+  },
+  {
+    path: '/register',
+    element: <RegisterPage/>,
+  },
+  {
+    path: '/login',
+    element: <LoginPage/>,
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+
+    <RouterProvider router={router}/>
+      
   </StrictMode>,
 )
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+
+function Homepage() {
+  return (
+    <div>
+      <h1>Welcome to our Homepage!</h1>
+      <p>This is a simple React app with a homepage component.</p>
+      <p>Click the button below to see a modal with a custom message.</p>
+    </div>
+  )
+}
+
+export default Homepage
