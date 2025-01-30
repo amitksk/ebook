a93858e46052f914de264e7a3054623d794bd0f7
amit013021 <amit013021@gmail.com>
2025-01-30 22:55:41 +0530

    add ebook logo

--- Backend/src/services/cloudinary.js
+++ Backend/src/services/cloudinary.js
@@ -2,13 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({
-  cloud_name: "dsianyb39",
-  api_key: "545611663964254",
-  api_secret: "n8jEz3PPjPjob_oKa1u-CIcL0qs",
-
-  // cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  // api_key: process.env.CLOUDINARY_API_KEY,
-  // api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
